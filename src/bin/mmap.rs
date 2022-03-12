0ca7ab021d735cd36d62c09cf145937c2416f614
Ian Hansen <221826+supershabam@users.noreply.github.com>
2022-03-11 20:44:49 -0700

    excessive calls but interface to memory

--- src/bin/mmap.rs
+++ src/bin/mmap.rs
@@ -1,15 +1,17 @@
-use std::fs::{OpenOptions, File};
+use std::fs::{File, OpenOptions};
+use core::ops::{Index,IndexMut};
+use std::io::Write;
 use std::path::PathBuf;
 
 use anyhow::Result;
 use memmap2::MmapMut;
 
 struct Buf {
-    mmap: Box<MmapMut>,
+    mmap: MmapMut,
 }
 
 impl Buf {
-    fn new() -> Result<Self> {
+    fn new() -> Result<Buf> {
         let path: PathBuf = PathBuf::from(r"test.bin");
         let file = OpenOptions::new()
             .read(true)
@@ -17,15 +19,31 @@ impl Buf {
             .create(true)
             .open(&path)?;
         file.set_len(4062)?;
-        let mut mmap = unsafe { Box::new(MmapMut::map_mut(&file)?) };
-        Ok(Buf{
+        let mmap = unsafe { MmapMut::map_mut(&file)? };
+        Ok(Buf { 
             mmap: mmap,
         })
     }
 }
 
+impl Index<usize> for Buf {
+    type Output = u64;
+    fn index(&self, index: usize) -> &Self::Output {
+        let (_, buf, _) = unsafe { self.mmap.align_to::<u64>() };
+        &buf[index]
+    }
+}
+
+impl IndexMut<usize> for Buf {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        let (_, buf, _) = unsafe { self.mmap.align_to_mut::<u64>() };
+        &mut buf[index]
+    }
+}
+
 fn main() -> Result<()> {
     let mut buf = Buf::new()?;
-    buf.mmap[3] = 8;
+    buf[5] = 0xdeadbeef;
+    println!("{}", buf[5]);
     Ok(())
 }
