5ba11ded36dbf6c627adb9ec8630a81fd6592923
Ian Hansen <221826+supershabam@users.noreply.github.com>
2022-04-02 15:58:34 -0600

    removes dead code

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use anyhow::Result;
 use bio::io::fasta::Reader;
 use image::ImageBuffer;
 
-use std::f64::consts::PI;
 use std::path::PathBuf;
 use std::time::Duration;
 use std::time::Instant;
@@ -60,130 +59,6 @@ fn filter_n(seq: &&[u8]) -> bool {
     true
 }
 
-#[cfg(test)]
-mod tests {
-    use crate::*;
-
-    #[test]
-    fn test_min_max_thetas() {
-        let width = 2;
-        let height = 2;
-        let x = 1;
-        let y = 1;
-        let (min, max) = min_max_thetas(width, height, x, y);
-
-        assert_eq!(min, 0.0);
-        assert_eq!(max, PI / 2.0);
-
-        let width = 200;
-        let height = 200;
-        let x = 110;
-        let y = 145;
-        let (min, max) = min_max_thetas(width, height, x, y);
-
-        assert_eq!(min, 1.33105321792444);
-        assert_eq!(max, 1.356735643231075);
-    }
-
-    #[test]
-    fn test_seqlen_from_position() {
-        let width = 200;
-        let height = 200;
-        let x = 100;
-        let y = 100;
-        let seqlen = seqlen_from_position(width, height, x, y, 90);
-
-        assert_eq!(seqlen, 1);
-
-        let width = 200;
-        let height = 200;
-        let x = 200;
-        let y = 100;
-        let seqlen = seqlen_from_position(width, height, x, y, 90);
-
-        assert_eq!(seqlen, 90);
-
-        let width = 200;
-        let height = 200;
-        let x = 190;
-        let y = 100;
-        let seqlen = seqlen_from_position(width, height, x, y, 100);
-
-        assert_eq!(seqlen, 95);
-    }
-}
-
-fn min_max_thetas(width: usize, height: usize, x: u32, y: u32) -> (f64, f64) {
-    let (min_theta, max_theta) = vec![(x, y), (x + 1, y), (x, y + 1), (x + 1, y + 1)]
-        .iter()
-        .fold((None, None), |(min, max), (x, y)| {
-            let x: i32 = *x as i32 - (width / 2) as i32;
-            let y: i32 = *y as i32 - (height / 2) as i32;
-            let theta = {
-                let mut theta = (y as f64 / x as f64).atan();
-                if x == 0 {
-                    theta = PI / 2.0;
-                }
-                if x < 0 {
-                    theta += PI;
-                }
-                if theta < 0.0 {
-                    theta = theta + PI + PI;
-                }
-                // rotate for effect; note, this complicates the assumptions on min/max for angles
-                // however, a min-max on sequence could still be done.
-                // theta = theta + PI / 7.23;
-                // if theta > 2.0 * PI {
-                //     theta = theta - 2.0 * PI;
-                // }
-                theta
-            };
-            let min = match min {
-                Some(current) => {
-                    if current < theta {
-                        Some(current)
-                    } else {
-                        Some(theta)
-                    }
-                }
-                None => Some(theta),
-            };
-            let max = match max {
-                Some(current) => {
-                    if current < theta {
-                        Some(theta)
-                    } else {
-                        Some(current)
-                    }
-                }
-                None => Some(theta),
-            };
-            (min, max)
-        });
-    let min_theta = min_theta.unwrap();
-    let max_theta = max_theta.unwrap();
-    (min_theta, max_theta)
-}
-
-fn seqlen_from_position(width: usize, height: usize, x: u32, y: u32, max_seqlen: usize) -> usize {
-    let x: i32 = x as i32 - (width / 2) as i32;
-    let y: i32 = y as i32 - (height / 2) as i32;
-    let r = ((x * x + y * y) as f64).sqrt();
-    let max = width as f64 / 2.0;
-    let p = r / max;
-    // println!("r={} max={} p={}", r, max, p);
-    let p = p.min(1.0);
-    let p = p.sqrt().sqrt(); // express smaller radii less
-    let interp = p * max_seqlen as f64;
-    // println!("p={} max_seqlen={} interp={}", p, max_seqlen, interp);
-    interp.ceil().max(1.0) as usize
-    // (p * max_seqlen as f64).ceil().min(1.0) as usize
-
-    // let max = width as f64 / max_seqlen as f64 / 2.0;
-
-    // ((r / max).floor() as usize + 1).min(max_seqlen)
-}
-
 fn main() {
     let args = Cli::parse();
     match &args.command {
@@ -229,8 +104,8 @@ fn print(index_file: &str, seqlen: usize, side_length: usize) -> Result<()> {
         let m = m.clone();
         let pixels = pixels.clone();
         thread::spawn(move || {
-            let mut acc = Accumulator::default();
             for p in pixels {
+                let mut acc = Accumulator::default();
                 let (gte, lt) = p.index_range();
                 let c = acc.sum_to(&m, gte, lt);
                 let val = (
@@ -243,28 +118,6 @@ fn print(index_file: &str, seqlen: usize, side_length: usize) -> Result<()> {
                 println!("sending point={:?} range=({}, {}) range=({}, {}) val={:?}", p, gte, lt, gtes, lts, val);
                 tx.send(val).unwrap();
             }
-            // let seqrange = make_coords_to_seq_range(width as usize, height as usize, seqlen);
-            // for y in 0..height {
-            //     for x in 0..width {
-            //         if x % thread_count != worker_id {
-            //             continue;
-            //         }
-            //         let testx: i32 = x as i32 - (width / 2) as i32;
-            //         let testy: i32 = y as i32 - (height / 2) as i32;
-            //         if (-3 < testx && testx < 3) || (-3 < testy && testy < 3) {
-            //             // println!("{} {}", min, max);
-            //             // TODO figure out why bright lines are forming on axis
-            //             continue;
-            //         }
-            //         let (min, max) = seqrange(x as u32, y as u32);
-
-            //         let len = min.len();
-            //         let c = m.count(seq_to_idx(min.as_bytes()), seq_to_idx(max.as_bytes()));
-            //         tx.send((x, y, c, len)).unwrap();
-            //     }
-            // }
-            // tx.send((0, 0, (0, 0), 0)).unwrap();
-            // println!("worker_id={} exiting", worker_id)
         });
     }
     println!("creating image");
@@ -298,7 +151,6 @@ fn print(index_file: &str, seqlen: usize, side_length: usize) -> Result<()> {
     println!("creating maxes buffer");
     for y in 0..height {
         for x in 0..width {
-            let seqlen = seqlen_from_position(width, height, x as u32, y as u32, seqlen);
             maxesbuf[y * height + x] = maxes[seqlen];
         }
     }
